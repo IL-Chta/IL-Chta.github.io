7a381a334df9fbd691fef82c11a9c8ba704e74ab
professorildebrandoleandro <professorildebrandoleandro@users.noreply.github.com>
2026-09-04 16:15:41 -0300

    Restaura rota internacional das chamadas

--- turn-config.js
+++ turn-config.js
@@ -1,1 +1,24 @@
-(()=>{const Native=window.RTCPeerConnection;const iceServers=[{urls:'stun:stun.relay.metered.ca:80'},{urls:'turn:global.relay.metered.ca:80',username:'debddc575de69bae142e9a8b',credential:'TYzdyA2ctn68KjLO'},{urls:'turn:global.relay.metered.ca:80?transport=tcp',username:'debddc575de69bae142e9a8b',credential:'TYzdyA2ctn68KjLO'},{urls:'turn:global.relay.metered.ca:443',username:'debddc575de69bae142e9a8b',credential:'TYzdyA2ctn68KjLO'},{urls:'turns:global.relay.metered.ca:443?transport=tcp',username:'debddc575de69bae142e9a8b',credential:'TYzdyA2ctn68KjLO'}];window.RTCPeerConnection=function(config={}){return new Native({...config,iceServers,iceCandidatePoolSize:10})};window.RTCPeerConnection.prototype=Native.prototype;})();
+(function () {
+  "use strict";
+  var NativePeerConnection = window.RTCPeerConnection;
+  if (!NativePeerConnection || window.__ilInternationalCallConfig) return;
+  window.__ilInternationalCallConfig = true;
+  var iceServers = [
+    { urls: "stun:stun.l.google.com:19302" },
+    { urls: "stun:stun1.l.google.com:19302" },
+    { urls: "stun:stun.relay.metered.ca:80" },
+    { urls: "turn:global.relay.metered.ca:80", username: "a5c3184b4a4836923cf6bd96", credential: "mcOUQqUdXie7PTd1" },
+    { urls: "turn:global.relay.metered.ca:80?transport=tcp", username: "a5c3184b4a4836923cf6bd96", credential: "mcOUQqUdXie7PTd1" },
+    { urls: "turn:global.relay.metered.ca:443", username: "a5c3184b4a4836923cf6bd96", credential: "mcOUQqUdXie7PTd1" },
+    { urls: "turns:global.relay.metered.ca:443?transport=tcp", username: "a5c3184b4a4836923cf6bd96", credential: "mcOUQqUdXie7PTd1" }
+  ];
+  function ILPeerConnection(configuration) {
+    return new NativePeerConnection(Object.assign({}, configuration || {}, {
+      iceServers: iceServers,
+      iceCandidatePoolSize: 10
+    }));
+  }
+  ILPeerConnection.prototype = NativePeerConnection.prototype;
+  if (NativePeerConnection.generateCertificate) ILPeerConnection.generateCertificate = NativePeerConnection.generateCertificate.bind(NativePeerConnection);
+  window.RTCPeerConnection = ILPeerConnection;
+})();
